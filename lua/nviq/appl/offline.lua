83ed83a0109de568947221afe56c0d30a8fb2656
Anthony <tjjzqwh@163.com>
2025-07-29 19:03:58 +0800

    feat: lsp config for offline mode

--- lua/nviq/appl/offline.lua
+++ lua/nviq/appl/offline.lua
@@ -9,9 +9,52 @@ vim.g.netrw_liststyle = 3
 vim.g.netrw_browse_split = 4
 vim.keymap.set("n", "<leader>op", "<Cmd>20Lexplore<CR>")
 
+-- LSP
+
+vim.lsp.config("clangd", {
+  cmd = { "clangd" },
+  filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+  root_markers = {
+    ".clangd",
+    ".clang-tidy",
+    ".clang-format",
+    "compile_commands.json",
+    "compile_flags.txt",
+    "configure.ac", -- AutoTools
+    ".git",
+  },
+  capabilities = {
+    textDocument = {
+      completion = {
+        editsNearCursor = true,
+      },
+    },
+    offsetEncoding = { "utf-8", "utf-16" },
+  },
+})
+
+vim.lsp.config("lua_ls", {
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  root_markers = {
+    ".luarc.json",
+    ".luarc.jsonc",
+    ".luacheckrc",
+    ".stylua.toml",
+    "stylua.toml",
+    "selene.toml",
+    "selene.yml",
+    ".git",
+  },
+})
+
 -- Completion
 
 vim.o.completeopt = "menu,menuone,noselect"
+
+require("nviq.appl.lsp").register_client_on_attach(function(client, bufnr)
+  vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
+end)
 
 lib.new_keymap("i", "<CR>", function(fallback)
   if vim.fn.pumvisible() ~= 0 then
@@ -22,9 +65,14 @@ lib.new_keymap("i", "<CR>", function(fallback)
 end)
 
 lib.new_keymap("i", "<Tab>", function(fallback)
-  if vim.fn.pumvisible() ~= 0 or
-      vim.regex("\\v[A-Za-z_\\u4e00-\\u9fa5]$"):match_str(lib.get_half_line(-1).b) then
+  if vim.fn.pumvisible() ~= 0 then
     lib.feedkeys("<C-N>", "n", true)
+  elseif vim.regex("\\v[A-Za-z_\\u4e00-\\u9fa5]$"):match_str(lib.get_half_line(-1).b) then
+    if vim.tbl_isempty(vim.lsp.get_clients { bufnr = 0 }) then
+      lib.feedkeys("<C-N>", "n", true)
+    else
+      vim.lsp.completion.get()
+    end
   else
     fallback()
   end
