4f9a3b152a8792c5260a1488862dab8fa1029d1f
AnthonyK213 <tjjzqwh@outlook.com>
2025-08-10 12:23:36 +0800

    feat: fuzzy file match for offline mode

--- lua/nviq/appl/offline.lua
+++ lua/nviq/appl/offline.lua
@@ -141,33 +141,188 @@ vim.keymap.set("n", "<leader>fb", ":buffer<space>", { desc = "Find buffer" })
 
 -- Find file
 
+---@class nviq.appl.offline.find_file.LcsInfo
+---@field len integer Length of the longest match.
+---@field i integer
+---@field j integer
+
+---@type nviq.appl.offline.find_file.LcsInfo
+local LCS_NIL = { len = 0, i = 0, j = 0 }
+
+---@class nviq.appl.offline.find_file.FileInfo
+---@field name string The relative file path.
+---@field score integer The matching score.
+
+---@class nviq.appl.offline.find_file.LcsMemo
+---@field private m_data nviq.appl.offline.find_file.LcsInfo[][]
+local LcsMemo = {}
+
+---@private
+LcsMemo.__index = LcsMemo
+
+---
+---@return nviq.appl.offline.find_file.LcsMemo
+function LcsMemo.new()
+  local lcs_memo = { m_data = {} }
+  setmetatable(lcs_memo, LcsMemo)
+  return lcs_memo
+end
+
+---
+---@param i integer
+---@param j integer
+---@return nviq.appl.offline.find_file.LcsInfo
+function LcsMemo:get(i, j)
+  return vim.tbl_get(self.m_data, i, j) or LCS_NIL
+end
+
+---
+---@param i integer
+---@param j integer
+---@param info nviq.appl.offline.find_file.LcsInfo
+function LcsMemo:set(i, j, info)
+  if not self.m_data[i] then
+    self.m_data[i] = {}
+  end
+  self.m_data[i][j] = info
+end
+
+---Extracts the matching position.
+---@param i integer
+---@param j integer
+---@return integer[]
+function LcsMemo:get_pos(i, j)
+  local pos = {}
+  local info = self:get(i, j)
+  while info.len > 0 do
+    pos[info.len] = info.i
+    info = self:get(info.i - 1, info.j - 1)
+  end
+  return pos
+end
+
+---
+---@param target string
+---@param query string[]
+---@return integer[]
+local function lcs(target, query)
+  local dp = LcsMemo.new()
+
+  local n1, n2 = #target, #query
+  for i = 1, n1 do
+    for j = 1, n2 do
+      if target:sub(i, i) == query[j] then
+        dp:set(i, j, {
+          len = dp:get(i - 1, j - 1).len + 1,
+          i = i,
+          j = j,
+        })
+      else
+        local a = dp:get(i - 1, j)
+        local b = dp:get(i, j - 1)
+        dp:set(i, j, (a.len >= b.len) and a or b)
+      end
+    end
+  end
+
+  return dp:get_pos(n1, n2)
+end
+
+---
+---@param match integer[]
+---@param target string
+---@return integer
+local function match_score(match, target)
+  local score = #match
+  -- Prefers continuous matching.
+  for i = 2, #match do
+    if match[i] - match[i - 1] == 1 then
+      score = score + 2
+    end
+  end
+  -- Prefers to match end to end.
+  local front = match[1] - 1
+  local prev_char = target:sub(front, front)
+  if prev_char == "" or prev_char == "/" then
+    score = score + 1
+  end
+  local back = match[#match] + 1
+  local next_char = target:sub(back, back)
+  if next_char == "" then
+    score = score + 1
+  end
+  return score
+end
+
+---@async
+---@param dir string
+---@return string[]?
+local function git_get_files(dir)
+  local futures = require("nviq.util.futures")
+
+  local ls_files = futures.Process.new("git", {
+    args = { "ls-files", "--cached", "--others", "--exclude-standard" },
+    cwd = dir,
+  })
+  ls_files:set_record(true)
+  local code, _ = ls_files:await()
+  if code ~= 0 then return end
+
+  return vim.iter(ls_files:stdout_buf()):map(function(item)
+    return vim.split(item, "\n", { plain = true, trimempty = true })
+  end):flatten():totable()
+end
+
 vim.keymap.set("n", "<leader>ff", function()
   local futures = require("nviq.util.futures")
 
   futures.spawn(function()
     local pattern = futures.ui.input { prompt = "Find file: " }
     if not pattern then return end
-    pattern = pattern:lower()
 
     local cwd = vim.fn.getcwd()
+    local files = git_get_files(cwd)
 
-    local items = vim.fs.find(function(name --[[@as string]], _)
-      return name:lower():match(pattern)
-    end, { path = cwd, type = "file", limit = 10 })
+    if not files then
+      files = {}
+      for name, type_ in vim.fs.dir(cwd, { depth = 42 }) do
+        if type_ == "file" then
+          table.insert(files, name)
+        end
+      end
+    end
 
-    if #items == 0 then
+    ---@type nviq.appl.offline.find_file.FileInfo[]
+    local file_infos = {}
+    local query = vim.split(pattern:lower(), "")
+
+    for _, file in ipairs(files) do
+      local pos = lcs(file:lower(), query)
+      if #pos > 0 then
+        table.insert(file_infos, {
+          name  = file,
+          score = match_score(pos, file),
+        })
+      end
+    end
+
+    if #file_infos == 0 then
       vim.notify("Nothing found")
       return
     end
 
-    local file_path = futures.ui.select(items, {
-      prompt = "Pick file: ",
-      format_item = function(item)
-        return vim.fs.relpath(cwd, item) or item
-      end
-    })
-    if not file_path then return end
+    table.sort(file_infos, function(a, b) return a.score > b.score end)
 
-    lib.edit_file(file_path)
+    -- Only show the first 10 matches.
+    file_infos = vim.iter(file_infos):take(10):totable()
+
+    ---@type nviq.appl.offline.find_file.FileInfo
+    local info = futures.ui.select(file_infos, {
+      prompt = "Pick file: ",
+      format_item = function(item) return item.name end
+    })
+    if not info then return end
+
+    lib.edit_file(vim.fs.joinpath(cwd, info.name))
   end)
 end, { desc = "Find file" })
