9cdf98917f3d30d7501d1bbf1c547003e9643bae
AnthonyK213 <tjjzqwh@outlook.com>
2025-12-22 21:58:03 +0800

    buggy netrw

--- lua/nviq/appl/offline.lua
+++ lua/nviq/appl/offline.lua
@@ -10,11 +10,12 @@ vim.o.mouse = "nvic"
 
 vim.g.netrw_altv = 1
 vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 80
-vim.g.netrw_liststyle = 3
+vim.g.netrw_winsize = 20
+vim.g.netrw_liststyle = 4
 vim.g.netrw_browse_split = 4
+vim.g.netrw_clipboard = 0
 
-vim.keymap.set("n", "<leader>op", "<Cmd>20Lexplore<CR>")
+vim.keymap.set("n", "<leader>op", "<Cmd>Lexplore<CR>")
 
 -- LSP
 -- From [nvim-lspconfig](https://github.com/neovim/nvim-lspconfig)
