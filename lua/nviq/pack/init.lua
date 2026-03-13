10e2aa7f0c45dd51040abd9315fe966bf9d260f5
Anthony <tjjzqwh@163.com>
2026-03-13 09:53:52 +0800

    feat: PacksUpdate

--- lua/nviq/pack/init.lua
+++ lua/nviq/pack/init.lua
@@ -16,3 +16,21 @@ require("nviq.pack.dev")
 require("nviq.pack.mark")
 
 packer.end_()
+
+vim.api.nvim_create_user_command("PacksUpdate", function(tbl)
+  local opt = { force = false }
+  if #tbl.fargs == 0 then
+    vim.pack.update(nil, opt)
+  else
+    vim.pack.update(tbl.fargs, opt)
+  end
+end, {
+  nargs = "*",
+  complete = function()
+    local plug_datas = vim.pack.get(nil, { info = false })
+    return vim.iter(plug_datas):map(function(plug_data)
+      return plug_data.spec.name
+    end):totable()
+  end,
+  desc = "Update packages managed by vim.pack."
+})
